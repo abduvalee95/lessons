7de14f1ac288f38df0bde0c0387e202064137941
leo <94227922+abduvalee95@users.noreply.github.com>
2021-11-21 13:06:04 +0900

    bu meniki
     yangi organganlarim

---
+++ jkghb/tartiblash.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 15:01:57 2021
+
+@author: as
+"""
+
+# cars = ['mers','tiko', 'lexus', 'audi', 'Benz']
+# sonlar = [12,14,10,21,15,]
+# narhlar=[1000,300,30000,5837,39488,8383,9947,]
+# arzon=min(narhlar)
+# qimmat=max(narhlar)
+# jami=sum(narhlar)
+# print('Eng arzon narh', arzon ,'Eng qimmat narh' ,qimmat ,' jami', jami )
+#  # KOPIROVAT
+# my_cars=cars # bu birhil ochish kopiya emas 
+# my_cars=car[:] #bu kopiya kilishtoy
+
+
+ #   TUPLE
+#toys = ('bus', 'car', 'cat', 'bear','robot', 'dino')
+#print(toys[0])
+#print(toys[-1])
+#print(toys[2:5])
+
+
+# mashinalar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai', ]
+# for avto in mashinalar:#mahsinalar ichidagi har bir avto uchun
+#     if avto =='bmw': #agar avto BMW ga teng bolsa
+#         print(avto.upper()) #avto matinini harifini katta bn  yoz
+#     else: # aks holda...
+#        print(avto.title()) #bosh harifini katta kilb yoz
+    
+    
+
---
+++ string.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 18:25:00 2021
+
+@author: as
+"""
+#ism = 'Ahmadillo'
+#familiya = 'TAZHIBAEV'
+#print(ism + familiya)
+#print(ism+' '+familiya)
+ 
+# f-string
+
+#ism = 'Ahmadillo'
+#familiya = 'Tazhibaev'
+#ism_sharif = f"{ism} {familiya}"
+#print (ism_sharif)
+
+#ism = "Ahmadillo"
+#familya= "Ahmad"
+#yosh='20'
+#print (f"Salom,😎 mening ismim {familiya}. {ism}.yoshim{yosh}da")
+ 
+#print ("hello world")
+#print("hello \tworld")
+#print("hello \nworld")
+
+
+
+#STRING METODLARI
+
+#ism='AHMAD'
+#familiya='TAZHIBAEV'
+#ism_sharif = f"{ism} {familiya}"
+#print (ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+#print(ism_sharif.title())
+
+#meva= '       olma        '
+#print("Men" + meva.rstrip()+"yahshi korib eyman")
+#print("men " + meva.strip() + " yahshi koraman")
+
+# INPUT
+#ism = ("leo")
+# ism = input('ismingiz nima?')
+# yosh = input('yoshingiz nechida?'+ ism )
+
+#print("Asslamu alaykum "+ ism.upper() + yosh  +' sizni nahorgi oshga taklif kilamiz' )
+#print("nima kilyabsiz " + ism.title())
+
+#cars = []
+
+
+
+
+
+
+
